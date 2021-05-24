e91891c404320a9cd415494ca46f4e9995e5c2fd
Narouski <Y.Narouski@gmail.com>
2021-05-24 13:00:05 +0300

    add class Admin in admin.py

--- books_dir/admin.py
+++ books_dir/admin.py
@@ -2,4 +2,16 @@ from django.contrib import admin
 from .models import Directory
 
 
+class DirectoryAdmin(admin.ModelAdmin):
+    list_display = [
+        'pk',
+        'book',
+        'author',
+        'genre',
+        'text',
+        'created_date',
+        'published_date'
+    ]
+
+
 admin.site.register(Directory)
